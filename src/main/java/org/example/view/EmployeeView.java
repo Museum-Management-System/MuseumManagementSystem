70db6e71cfb683a3b51fb6d874dc5405bb02eca5
Barış Yapıcı <baris.yapici@ozu.edu.tr>
2024-12-14 23:35:49 +0300

    14.12.24 Problems on EmployeeView has been solved.

--- src/main/java/org/example/view/EmployeeView.java
+++ src/main/java/org/example/view/EmployeeView.java
@@ -1,4 +1,5 @@
 package org.example.view;
+
 import javax.swing.*;
 import java.awt.*;
 import java.text.ParseException;
@@ -6,24 +7,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-//There might be some small problems will be checked.
 public class EmployeeView extends JFrame {
     private JTable employeeTable;
     private JButton addButton, updateButton, deleteButton, searchButton;
-    private JTextField nameField, surNameField, passwordField, entranceDateField;
+    private JTextField IDField ,nameField, emailField, phoneNumField, jobTitleField, sectionNameField, roleField;
     private JTextField searchField;
     private JTabbedPane tabbedPane;
 
     private JPanel createEmployeePanel, getEmployeePanel, searchByIDPanel;
 
-    private JLabel idLabel, nameLabel, surnameLabel, entranceDateLabel;
-    private JTextField idField, nameFieldResult, surnameFieldResult, entranceDateFieldResult;
+    private JLabel idLabel, nameLabel, emailLabel, phoneNumLabel, jobTitleLabel, sectionNameLabel, roleLabel;
+    private JTextField idField, nameFieldResult, emailFieldResult, phoneNumFieldResult, jobTitleFieldResult, sectionNameFieldResult, roleFieldResult, entranceDateFieldResult;
 
     private JLabel messageLabel;
 
     public EmployeeView() {
         setTitle("Museum Management System - Employee");
-        setSize(600, 500); // Adjusted size for the content
+        setSize(700, 600); // Adjusted size for the content
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -39,22 +39,38 @@ public class EmployeeView extends JFrame {
 
         add(tabbedPane, BorderLayout.CENTER);
 
-        JPanel createInputPanel = new JPanel(new GridLayout(5, 2));
+        // Create Employee Panel
+        JPanel createInputPanel = new JPanel(new GridLayout(9, 2));
+
+        createInputPanel.add(new JLabel("ID:"));
+        IDField = new JTextField();
+        createInputPanel.add(IDField);
+
+
         createInputPanel.add(new JLabel("Name:"));
         nameField = new JTextField();
         createInputPanel.add(nameField);
 
-        createInputPanel.add(new JLabel("Surname:"));
-        surNameField = new JTextField();
-        createInputPanel.add(surNameField);
+        createInputPanel.add(new JLabel("Email:"));
+        emailField = new JTextField();
+        createInputPanel.add(emailField);
 
-        createInputPanel.add(new JLabel("Password:"));
-        passwordField = new JTextField();
-        createInputPanel.add(passwordField);
+        createInputPanel.add(new JLabel("Phone Number:"));
+        phoneNumField = new JTextField();
+        createInputPanel.add(phoneNumField);
 
-        createInputPanel.add(new JLabel("Entrance Date (YYYY-MM-DD):"));
-        entranceDateField = new JTextField();
-        createInputPanel.add(entranceDateField);
+        createInputPanel.add(new JLabel("Job Title:"));
+        jobTitleField = new JTextField();
+        createInputPanel.add(jobTitleField);
+
+        createInputPanel.add(new JLabel("Section Name:"));
+        sectionNameField = new JTextField();
+        createInputPanel.add(sectionNameField);
+
+        createInputPanel.add(new JLabel("Role:"));
+        roleField = new JTextField();
+        createInputPanel.add(roleField);
+
 
         addButton = new JButton("Add Employee");
         createInputPanel.add(addButton);
@@ -65,6 +81,7 @@ public class EmployeeView extends JFrame {
 
         createEmployeePanel.add(createInputPanel, BorderLayout.CENTER);
 
+        // Get Employee Panel
         JPanel getInputPanel = new JPanel(new BorderLayout());
         JPanel searchPanel = new JPanel(new FlowLayout());
         searchPanel.add(new JLabel("Enter Employee Name:"));
@@ -78,7 +95,7 @@ public class EmployeeView extends JFrame {
 
         getEmployeePanel.add(getInputPanel, BorderLayout.NORTH);
 
-        JPanel detailsPanel = new JPanel(new GridLayout(4, 2));
+        JPanel detailsPanel = new JPanel(new GridLayout(8, 2));
 
         idLabel = new JLabel("ID: ");
         detailsPanel.add(idLabel);
@@ -92,36 +109,51 @@ public class EmployeeView extends JFrame {
         nameFieldResult.setEditable(false);
         detailsPanel.add(nameFieldResult);
 
-        surnameLabel = new JLabel("Surname: ");
-        detailsPanel.add(surnameLabel);
-        surnameFieldResult = new JTextField();
-        surnameFieldResult.setEditable(false);
-        detailsPanel.add(surnameFieldResult);
+        emailLabel = new JLabel("Email: ");
+        detailsPanel.add(emailLabel);
+        emailFieldResult = new JTextField();
+        emailFieldResult.setEditable(false);
+        detailsPanel.add(emailFieldResult);
 
-        entranceDateLabel = new JLabel("Entrance Date: ");
-        detailsPanel.add(entranceDateLabel);
-        entranceDateFieldResult = new JTextField();
-        entranceDateFieldResult.setEditable(false);
-        detailsPanel.add(entranceDateFieldResult);
+        phoneNumLabel = new JLabel("Phone Number: ");
+        detailsPanel.add(phoneNumLabel);
+        phoneNumFieldResult = new JTextField();
+        phoneNumFieldResult.setEditable(false);
+        detailsPanel.add(phoneNumFieldResult);
+
+        jobTitleLabel = new JLabel("Job Title: ");
+        detailsPanel.add(jobTitleLabel);
+        jobTitleFieldResult = new JTextField();
+        jobTitleFieldResult.setEditable(false);
+        detailsPanel.add(jobTitleFieldResult);
+
+        sectionNameLabel = new JLabel("Section Name: ");
+        detailsPanel.add(sectionNameLabel);
+        sectionNameFieldResult = new JTextField();
+        sectionNameFieldResult.setEditable(false);
+        detailsPanel.add(sectionNameFieldResult);
+
+        roleLabel = new JLabel("Role: ");
+        detailsPanel.add(roleLabel);
+        roleFieldResult = new JTextField();
+        roleFieldResult.setEditable(false);
+        detailsPanel.add(roleFieldResult);
 
         getEmployeePanel.add(detailsPanel, BorderLayout.CENTER);
 
         employeeTable = new JTable();
         getEmployeePanel.add(new JScrollPane(employeeTable), BorderLayout.SOUTH);
 
+        // Search by ID Panel
         JPanel idSearchPanel = new JPanel(new FlowLayout());
         idSearchPanel.add(new JLabel("Enter Employee ID:"));
-        entranceDateField = new JTextField(20);
-        idSearchPanel.add(entranceDateField);
+        JTextField searchByIDField = new JTextField(20);
+        idSearchPanel.add(searchByIDField);
 
         searchByIDPanel.add(idSearchPanel, BorderLayout.NORTH);
 
         employeeTable = new JTable();
         searchByIDPanel.add(new JScrollPane(employeeTable), BorderLayout.CENTER);
-    }
-
-    public String getSearchEmployeeIDInput() {
-        return entranceDateField.getText();
     }
 
     public JTable getEmployeeTable() {
@@ -144,40 +176,43 @@ public class EmployeeView extends JFrame {
         return nameField.getText();
     }
 
-    public String getEmployeeSurnameInput() {
-        return surNameField.getText();
+    public String getEmployeeEmailInput() {
+        return emailField.getText();
     }
 
-    public String getEmployeePasswordInput() {
-        return passwordField.getText();
+    public String getEmployeePhoneNumInput() {
+        return phoneNumField.getText();
     }
 
-    public Date getEmployeeEntranceDateInput() {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            return dateFormat.parse(entranceDateField.getText()); // Parse the date from text
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null; // Return null if parsing fails
-        }
+    public String getEmployeeJobTitleInput() {
+        return jobTitleField.getText();
     }
 
-    public String getSearchFieldInput() {
-        return searchField.getText();
+    public String getEmployeeSectionNameInput() {
+        return sectionNameField.getText();
     }
 
-    public void setEmployeeDetails(String id, String name, String surname, String entranceDate) {
+    public String getEmployeeRoleInput() {
+        return roleField.getText();
+    }
+
+    public void setEmployeeDetails(String id, String name, String email, String phoneNum, String jobTitle, String sectionName, String role) {
         idField.setText(id);
         nameFieldResult.setText(name);
-        surnameFieldResult.setText(surname);
-        entranceDateFieldResult.setText(entranceDate);
+        emailFieldResult.setText(email);
+        phoneNumFieldResult.setText(phoneNum);
+        jobTitleFieldResult.setText(jobTitle);
+        sectionNameFieldResult.setText(sectionName);
+        roleFieldResult.setText(role);
     }
 
     public void clearInputFields() {
         nameField.setText("");
-        surNameField.setText("");
-        passwordField.setText("");
-        entranceDateField.setText("");
+        emailField.setText("");
+        phoneNumField.setText("");
+        jobTitleField.setText("");
+        sectionNameField.setText("");
+        roleField.setText("");
         searchField.setText("");
     }
 
@@ -186,7 +221,7 @@ public class EmployeeView extends JFrame {
     }
 
     public void updateEmployeeTable(List<String[]> tableData) {
-        String[] columnNames = {"ID", "Name", "Surname", "Password", "Entrance Date"};
+        String[] columnNames = {"ID", "Name", "Email", "Phone Number", "Job Title", "Section Name", "Role", "Entrance Date"};
 
         Object[][] data = tableData.toArray(new Object[0][0]);
 
